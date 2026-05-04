0fceb7b5c18ca327901cac49d18dee61b1166c5d
monchai <koohza01@gmail.com>
2026-05-05 03:19:23 +0700

    ccs

--- app/Main.go
+++ app/Main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from Go service image2gi!")
+	fmt.Fprintf(w, "Hello from Go service image33333gi!")
 }
 
 func main() {
