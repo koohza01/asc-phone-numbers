4f0779c0a1302c2464f4110a15664cb22414d4a3
monchai <koohza01@gmail.com>
2026-07-15 23:22:07 +0700

    testupdate

--- app/Main.go
+++ app/Main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from Go service image33333gi!")
+	fmt.Fprintf(w, "Hello from Go service image33322333gi!")
 }
 
 func main() {
